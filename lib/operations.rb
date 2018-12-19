da211b58b5ee0b614e87e7b80c1ff5e5a40daab5
Kyle Houghton <kylebenz@hotmail.com>
2018-12-19 00:57:49 +0000

    All tests pass.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,23 @@
+
+
+##Simpler way:
+##
+##def unsafe?(speed)
+##  (speed < 40) || (speed > 60)
+##end
+
+##Following instructions:
+##
+
 def unsafe?(speed)
-
+  if (speed < 40) || (speed > 60)
+    true
+  else
+    false
+  end
 end
-
 
 
 def not_safe?(speed)
-	
+  (speed < 40) || (speed > 60) ? true : false
 end
-	
-
-
