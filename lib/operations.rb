41d74f784ee22b4283d2780f9fece63a76ff34c9
Kyle Houghton <kylebenz@hotmail.com>
2018-12-19 00:54:48 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -13,9 +13,10 @@ def unsafe?(speed)
     true
   else
     false
+  end
 end
 
 
-#def not_safe?(speed)
-#  (speed < 40) || (speed > 60) ? true : false
-#end
+def not_safe?(speed)
+  (speed < 40) || (speed > 60) ? true : false
+end
